3c661b2a6228a0d0c25eb7ed1c35b14e0df83a78
glconde <georgelouie.conde@edu.sait.ca>
2025-09-18 16:24:51 -0600

    solution

---
+++ README.md
@@ -0,0 +1,22 @@
+## Code Challenge
+
+This repository contains my solution for a specific coding challenge. 
+
+### Program Description
+
+The program generates numbers from 1 to 10,000 in random order each time it is run, ensuring all values are **unique** and **within the required range**. 
+
+I used the Fisher–Yates shuffle algorithm, which runs in O(N) time and O(1) extra space, making it efficient and reliable. 
+
+### Extra / Validation
+
+The code includes a simple validation step to confirm correctness and is written in JavaScript for easy testing in Node.js (or other platform / tool).
+
+### Running via CLI
+```bash
+node shuffle.js
+```
+
+
+### Demo
+Alternatively, live demo is also available here: https://jsfiddle.net/glconde/2br5ov1y/17/
---
+++ shuffle.js
@@ -0,0 +1,33 @@
+// define range
+const N = 10000;
+
+// generate the sequence from 1 to N, will be guaranteed unique
+// time complexity: O(N), space complexity: O(N)
+const sequence = Array.from({ length: N }, (_, i) => i + 1);
+
+// verify the sequence
+console.log(sequence);
+
+// fisher-yates shuffler
+// time complexity: O(N), space complexity: O(1)
+function shuffleArray(array) {
+  for (let i = array.length - 1; i >= 1; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+// output
+let shuffledResult = shuffleArray(sequence);
+console.log(shuffledResult);
+
+// not required but just to validate / sanity check
+// range N is preserved, all numbers are unique, all numbers are within [1..N]
+const isValid =
+  shuffledResult.length === N &&
+  new Set(shuffledResult).size === N &&
+  Math.min(...shuffledResult) === 1 &&
+  Math.max(...shuffledResult) === N;
+
+console.log("valid result: ", isValid);
